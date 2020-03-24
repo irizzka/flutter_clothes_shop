ecdcc55da3410711191ffa1915657dd00dfa5151
ingvar <ingvarCherevko@gmail.com>
2020-03-25 01:37:51 +0200

    second commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,15 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
   final _firestore = Firestore.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: RaisedButton(
         onPressed: () {
-          _firestore.collection('test').add({
-            'data' : 'ccawca'
+          _firestore.collection('clothes-man/man-teenager/jeans').add({
+            'description': 'description2',
+            'imageUrl': "non",
+            'name': 'new name',
+            'price': 200
           });
         },
       ),
